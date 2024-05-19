5cb1e54fdd4db7f81c28a77b04e3a851e748c8e2
RedYou <jcdemers123@gmail.com>
2024-05-18 22:31:08 -0400

    select options

--- lib/src/query/select.rs
+++ lib/src/query/select.rs
@@ -5,6 +5,10 @@ use crate::table::Column;
 pub struct Select {
     columns: HashMap<&'static str, Vec<Column>>,
     tables_join: Vec<String>,
+    wheres: Option<String>,
+    orderbys: Option<String>,
+    limit: Option<usize>,
+    offset: Option<usize>,
 }
 
 impl Select {
@@ -13,6 +17,10 @@ impl Select {
         Self {
             columns,
             tables_join,
+            wheres: None,
+            orderbys: None,
+            limit: None,
+            offset: None,
         }
     }
 }
@@ -20,7 +28,7 @@ impl Select {
 impl ToString for Select {
     fn to_string(&self) -> String {
         format!(
-            "SELECT {} FROM {};",
+            "SELECT {} FROM {}{}{}{}{};",
             self.columns
                 .iter()
                 .flat_map(|(key, values)| values
@@ -29,7 +37,38 @@ impl ToString for Select {
                     .collect::<Vec<String>>())
                 .collect::<Vec<String>>()
                 .join(", "),
-            self.tables_join.join(" ")
+            self.tables_join.join(" "),
+            self.wheres
+                .as_ref()
+                .map_or(String::new(), |s| format!(" WHERE {s}")),
+            self.orderbys
+                .as_ref()
+                .map_or(String::new(), |s| format!(" ORDER BY {s}")),
+            self.limit
+                .as_ref()
+                .map_or(String::new(), |s| format!(" LIMIT {s}")),
+            self.offset
+                .as_ref()
+                .map_or(String::new(), |s| format!(" OFFSET {s}"))
         )
+    }
+}
+
+impl Select {
+    pub fn wheres(&mut self, wheres: String) -> &mut Self {
+        self.wheres = Some(wheres);
+        self
+    }
+    pub fn orderbys(&mut self, orderbys: String) -> &mut Self {
+        self.orderbys = Some(orderbys);
+        self
+    }
+    pub fn limit(&mut self, limit: usize) -> &mut Self {
+        self.limit = Some(limit);
+        self
+    }
+    pub fn offset(&mut self, offset: usize) -> &mut Self {
+        self.offset = Some(offset);
+        self
     }
 }
--- tests/src/main.rs
+++ tests/src/main.rs
@@ -9,14 +9,14 @@ use rust_query_mysql::mysqlrow::MySQLRow;
 
 use crate::connection::get_conn;
 
-#[derive(Debug, Table, MySQLRow)]
+#[derive(Debug, Table, MySQLRow, PartialEq, Eq)]
 struct Test {
     #[PrimaryKey]
     id: i32,
     nom: String,
 }
 
-#[derive(Debug, Table, MySQLRow)]
+#[derive(Debug, Table, MySQLRow, PartialEq, Eq)]
 #[table_name("prof")]
 struct Test2 {
     #[PrimaryKey]
@@ -24,7 +24,7 @@ struct Test2 {
     nom: Option<String>,
 }
 
-#[derive(Debug, Table, MySQLRow)]
+#[derive(Debug, Table, MySQLRow, PartialEq, Eq)]
 #[table_name("test_prof")]
 struct Test3 {
     #[PrimaryKey]
@@ -36,24 +36,86 @@ struct Test3 {
     score: i32,
 }
 
-#[allow(clippy::missing_errors_doc)]
+#[allow(clippy::missing_errors_doc, clippy::missing_panics_doc)]
 pub fn main() -> Result<(), Error> {
-    println!(
-        "{:?}",
-        Test3::columns()
-            .iter()
-            .map(Column::name)
-            .collect::<Vec<&'static str>>()
-    );
-    println!("{}", Test3::select_all().to_string());
-
     let mut conn = get_conn()?;
+    println!("Testing");
 
-    let data = conn.query_map(Test3::select_all().to_string(), Test3::from_row)?;
+    let mut select_all = Test3::select_all();
+    let mut select_all = &mut select_all;
+    assert_eq!(
+        conn.query_map(select_all.to_string(), Test3::from_row).ok(),
+        Some(vec![data(0), data(1), data(2), data(3)])
+    );
 
-    for data in data {
-        println!("{data:?}");
-    }
+    select_all = select_all.wheres("prof.id > 1".to_owned());
+    assert_eq!(
+        conn.query_map(select_all.to_string(), Test3::from_row).ok(),
+        Some(vec![data(1), data(3)])
+    );
 
+    select_all = select_all.orderbys("test_prof.score DESC".to_owned());
+    assert_eq!(
+        conn.query_map(select_all.to_string(), Test3::from_row).ok(),
+        Some(vec![data(3), data(1)])
+    );
+
+    select_all = select_all.limit(1);
+    assert_eq!(
+        conn.query_map(select_all.to_string(), Test3::from_row).ok(),
+        Some(vec![data(3)])
+    );
+
+    select_all = select_all.offset(1);
+    assert_eq!(
+        conn.query_map(select_all.to_string(), Test3::from_row).ok(),
+        Some(vec![data(1)])
+    );
+
+    println!("Success");
     Ok(())
+}
+
+fn data(index: usize) -> Test3 {
+    match index {
+        0 => Test3 {
+            test: Test {
+                id: 1,
+                nom: "A".to_owned(),
+            },
+            prof: Test2 { id: 1, nom: None },
+            score: 1,
+        },
+        1 => Test3 {
+            test: Test {
+                id: 1,
+                nom: "A".to_owned(),
+            },
+            prof: Test2 {
+                id: 2,
+                nom: Some("B".to_owned()),
+            },
+            score: 2,
+        },
+        2 => Test3 {
+            test: Test {
+                id: 2,
+                nom: "B".to_owned(),
+            },
+            prof: Test2 { id: 1, nom: None },
+            score: 3,
+        },
+        3 => Test3 {
+            test: Test {
+                id: 2,
+                nom: "B".to_owned(),
+            },
+            prof: Test2 {
+                id: 2,
+                nom: Some("B".to_owned()),
+            },
+            score: 4,
+        },
+        _ => panic!("data not found"),
+    }
 }
